67f46e7b3e9389e3351aea869ac7040f837d0e5c
Daniel Cifuentes Mac <danielguarnizo@gmail.com>
2020-02-17 07:40:36 -0500

    Added ajax datatables on product index. Also changed language to es on config.

--- public/js/main.js
+++ public/js/main.js
@@ -1,21 +1,5 @@
-const products = document.getElementById('products');
+
 const categories = document.getElementById('categories');
-
-if (products) {
-    products.addEventListener('click', (e) => {
-        if (e.target.className === 'btn btn-danger delete-product') {
-            if (confirm('¿Está seguro?')) {
-                const id = e.target.getAttribute('data-id');
-
-                fetch(`/products/${id}/delete`, {
-                    method: 'DELETE'
-                }).then(response => {
-                    this.location.reload();
-                });
-            }
-        }
-    });
-}
 
 if (categories) {
     categories.addEventListener('click', (e) => {
--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -5,6 +5,11 @@ namespace App\Controller;
 use App\Entity\Product;
 use App\Form\Type\ProductType;
 use Doctrine\DBAL\Exception\ConstraintViolationException;
+use Doctrine\ORM\QueryBuilder;
+use Omines\DataTablesBundle\Adapter\Doctrine\ORMAdapter;
+use Omines\DataTablesBundle\Column\TextColumn;
+use Omines\DataTablesBundle\Column\TwigColumn;
+use Omines\DataTablesBundle\DataTableFactory;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
@@ -14,12 +19,42 @@ class ProductController extends AbstractController
 {
 
     /**
-     * @Route("/", methods={"GET","HEAD"})
+     * @Route("/", methods={"GET","HEAD","POST"})
      */
-    public function index()
+    public function index(Request $request, DataTableFactory $dataTableFactory)
     {
+        $table = $dataTableFactory->create()
+            ->add('code', TextColumn::class, ['label' => 'Código'])
+            ->add('name', TextColumn::class, ['label' => 'Nombre'])
+            ->add('description', TextColumn::class, ['label' => 'Descripción'])
+            ->add('brand', TextColumn::class, ['label' => 'Marca'])
+            ->add('category', TextColumn::class, ['field' => 'category.name', 'label' => 'Categoría'])
+            ->add('price', TextColumn::class, ['label' => 'Precio'])
+            ->add('id', TwigColumn::class, [
+                'orderable' => false,
+                'label' => '',
+                'className' => 'buttons',
+                'template' => 'products/buttonbar.html.twig',
+            ])
+            ->createAdapter(ORMAdapter::class, [
+                    'entity' => Product::class,
+                    'query' => function (QueryBuilder $builder) {
+                        $builder
+                            ->select('product')
+                            ->from(Product::class, 'product')
+                            ->join('product.category', 'category')
+                            ->andWhere('category.active = :val')
+                            ->setParameter('val', 1);
+                    }]
+            )
+            ->handleRequest($request);
+
+        if ($table->isCallback()) {
+            return $table->getResponse();
+        }
+
         $products = $this->getDoctrine()->getRepository(Product::class)->findAllActive();
-        return $this->render('products/index.html.twig', array('products' => $products));
+        return $this->render('products/index.html.twig', array('products' => $products, 'datatable' => $table));
     }
 
 
